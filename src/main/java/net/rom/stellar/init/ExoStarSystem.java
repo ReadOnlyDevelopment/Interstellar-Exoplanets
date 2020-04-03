60eb7d0d430c804f52f1e38a9680f7265aa8d5d5
ROMVoid95 <rom.void95@gmail.com>
2020-04-03 06:57:27 -0500

    make changes to system builder

--- src/main/java/net/rom/stellar/init/ExoStarSystem.java
+++ src/main/java/net/rom/stellar/init/ExoStarSystem.java
@@ -1,9 +1,15 @@
 package net.rom.stellar.init;
 
+import asmodeuscore.api.dimension.IAdvancedSpace.ClassBody;
+import asmodeuscore.api.dimension.IAdvancedSpace.StarColor;
+import asmodeuscore.api.dimension.IAdvancedSpace.TypeBody;
+import asmodeuscore.core.astronomy.BodiesData;
+import asmodeuscore.core.astronomy.BodiesHelper;
 import micdoodle8.mods.galacticraft.api.galaxies.SolarSystem;
 import micdoodle8.mods.galacticraft.api.vector.Vector3;
 import net.rom.core.space.AstroBuilder;
 import net.rom.stellar.Exoplanets;
+import net.rom.stellar.util.ModCheckUtil;
 
 public class ExoStarSystem {
 	
@@ -17,7 +23,14 @@ public class ExoStarSystem {
 
 	private static void registerSolarSystems() {
 		
-		YZCETI = b.buildSolarSystem("yzceti", "milky_way", new Vector3(1.0F,  1.0F, 0.0F), "yzcetia");
+		YZCETI = b.buildSolarSystem("yzceti", "milky_way", new Vector3(1.0F,  1.0F, 0.0F), ExoHostStar.yzcetia);
+		
+		// ######################## SET ASMODEUSCORE MAP DATA  ########################
+		
+				if(ModCheckUtil.asmodeusCoreLoaded()) {
+					BodiesData data = new BodiesData(TypeBody.STAR, ClassBody.DWARF).setStarColor(StarColor.RED);
+					BodiesHelper.registerBody(ExoHostStar.yzcetia, data , false);
+				}
 	}
 
 	private static void initializeSolarSystems() {
