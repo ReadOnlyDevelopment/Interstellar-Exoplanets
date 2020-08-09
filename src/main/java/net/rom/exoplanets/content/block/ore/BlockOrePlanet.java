5c5f8ccf8549809ac314181a951704c0aae77ee2
ROMVoid95 <rom.void95@gmail.com>
2020-08-09 10:46:00 -0500

    Fixed: Trappist 1E Ores Incorrect Placement

--- src/main/java/net/rom/exoplanets/content/block/ore/BlockOrePlanet.java
+++ src/main/java/net/rom/exoplanets/content/block/ore/BlockOrePlanet.java
@@ -1,46 +1,10 @@
 package net.rom.exoplanets.content.block.ore;
 
-import java.util.Locale;
+public class BlockOrePlanet extends BlockOreMetal {
 
-import micdoodle8.mods.galacticraft.core.blocks.ISortableBlock;
-import micdoodle8.mods.galacticraft.core.util.EnumSortCategoryBlock;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyEnum;
-import net.minecraft.block.state.BlockStateContainer;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.IStringSerializable;
-import net.minecraft.util.math.MathHelper;
-import net.minecraftforge.client.model.ModelLoader;
-import net.rom.exoplanets.ExoInfo;
-import net.rom.exoplanets.content.block.BlockExoOre;
-import net.rom.exoplanets.content.block.BlockGeneral;
-import net.rom.exoplanets.internal.RecipeBuilder;
-import net.rom.exoplanets.internal.inerf.ICustomModel;
-import net.rom.exoplanets.internal.inerf.item.ItemBlockMetaSubtypes;
-import net.rom.exoplanets.util.CreativeExoTabs;
-
-public class BlockOrePlanet extends BlockGeneral {
-	
-	public BlockOrePlanet(int level) {
-		super(Material.IRON);
-        this.setHardness(3.0f);
-        this.setResistance(30.0f);
-        this.setHarvestLevel("pickaxe", level);
-        this.setSoundType(SoundType.STONE);
+	public BlockOrePlanet(String name, boolean showInTab) {
+		super(name, showInTab);
+		// TODO Auto-generated constructor stub
 	}
-	
-    public ItemStack getStack(int count) {
-        return new ItemStack(this, count);
-    }
-
-	@Override
-	public void addRecipes(RecipeBuilder recipes) {
-		
-	}
-
 
 }
