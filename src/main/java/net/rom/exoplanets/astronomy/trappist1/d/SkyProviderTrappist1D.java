db63294d4e1759f2beb5edf4d31c9d57bf9874d1
ROMVoid95 <rom.void95@gmail.com>
2020-08-18 15:21:17 -0500

    Add: Storms To Trappist 1D

--- src/main/java/net/rom/exoplanets/astronomy/trappist1/d/SkyProviderTrappist1D.java
+++ src/main/java/net/rom/exoplanets/astronomy/trappist1/d/SkyProviderTrappist1D.java
@@ -17,8 +17,6 @@
 
 package net.rom.exoplanets.astronomy.trappist1.d;
 
-import org.lwjgl.opengl.GL11;
-
 import asmodeuscore.api.dimension.IAdvancedSpace.StarColor;
 import asmodeuscore.core.astronomy.sky.SkyProviderBase;
 import micdoodle8.mods.galacticraft.api.vector.Vector3;
@@ -26,30 +24,23 @@ import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.ResourceLocation;
 import net.rom.exoplanets.Assets;
-import net.rom.exoplanets.util.RGB;
 
 public class SkyProviderTrappist1D extends SkyProviderBase {
 
 	@Override
 	protected void rendererSky (Tessellator tessellator, BufferBuilder buffer, float f10, float ticks) {
-		GL11.glEnable(GL11.GL_BLEND);
 
-		GL11.glDisable(GL11.GL_BLEND);
+		this.starList = 0;
 	}
 
 	@Override
 	protected ModeLight modeLight () {
-		return ModeLight.DEFAULT;
+		return ModeLight.WITHOUT_SUN;
 	}
 
 	@Override
 	protected boolean enableBaseImages () {
-		return true;
-	}
-
-	@Override
-	protected float sunSize () {
-		return 13.0F;
+		return false;
 	}
 
 	@Override
@@ -59,7 +50,7 @@ public class SkyProviderTrappist1D extends SkyProviderBase {
 
 	@Override
 	protected boolean enableStar () {
-		return true;
+		return false;
 	}
 
 	@Override
@@ -69,13 +60,12 @@ public class SkyProviderTrappist1D extends SkyProviderBase {
 
 	@Override
 	protected Vector3 getAtmosphereColor () {
-		RGB c = RGB.parse("SkyBlue");
-		return new Vector3(c.getRed(), c.getGreen(), c.getBlue());
+		return new Vector3(0, 0, 0);
 	}
 
 	@Override
-	public int expandSizeAura () {
-		return 6;
+	protected float sunSize () {
+		return 0;
 	}
 
 }
